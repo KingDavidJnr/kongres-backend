eaf2ddfb12fe30e5f3b36aa4f7d043d1acb9618b
David Oduse <odusedavid@gmail.com>
2025-08-11 22:10:20 +0100

    [chore] update CORS config

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.use(
       "https://kongres.lovable.app",
       "https://kongres.vercel.app",
       "https://kongres.app",
+      "http://localhost:8080/",
     ],
     credentials: true, // Allow cookies and authentication headers
   })
